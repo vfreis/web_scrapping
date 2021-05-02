32a0dc57c145e8d96f226abdc54521848b7b5cd0
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-05-02 18:43:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,30 +11,28 @@ async function init(url) {
 
         let artist = document.querySelector('.SongHeader__Artist-sc-1b7aqpg-9')
         let song = document.querySelector('.SongHeader__Title-sc-1b7aqpg-7')
-        let featuring = document.querySelector('.metadata_unit-info')
-        //let producedby = document.querySelector('.act-show cont-songs snarly')
 
         /*
-            adicionar os novos valores
+            outras infos
         */
 
         if (!artist && !song  && !featuring) {
             artist = document.querySelector('.header_with_cover_art-primary_info-primary_artist')
             song = document.querySelector('.header_with_cover_art-primary_info-title')
-            featuring = document.querySelector('.HeaderMetadata__Section-sc-1p42fnf-2')
-            //producedby = document.querySelector('.act-show cont-songs snarly')
+
         }
 
         return {
             artist: artist.innerText,
             song: song.innerText,
-            featuring: featuring.innerText,
-            //producedby: producedby.innerText
+
         }
+
+        browser.close()
 
     })
 
-
+    console.log('Info:', values)
 
 }
 
